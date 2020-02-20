c9c6e93c172adf723908889c101a972a26350b3b
StevengerOffi <nat.icart79@gmail.com>
2020-02-20 13:39:30 +0100

    Création de TP entre les scènes, bug avec les lumières dans la chambre de départ et les lamps du couloirs ( clignotement )

---
+++ Endless Nightmare/Assets/Script/SceneTP1.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTP1 : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+            SceneManager.LoadScene(2);
+    }
+}
